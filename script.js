c7a564e795a48570177af6dc56b8e80a9b1447c3
octaviolobo <163486842+octaviolobo@users.noreply.github.com>
2024-04-16 13:12:35 -0700

    comments

--- script.js
+++ script.js
@@ -1,6 +1,8 @@
+//pulling the button by class
 let switch_button = document.getElementsByClassName ('dark-mode-button');
-
+// adding event listerner to know the status of the switch
 switch_button.addEventListener('chane', function(){
+   // functionality of the button 
     if (switch_button.checked){
         console.log('switch on');
     } 
